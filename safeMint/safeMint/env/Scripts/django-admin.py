86c68d681c1baecb174bd4ec673bae64c00b9844
theuserman <ilinan87@gmail.com>
2018-04-03 14:52:19 -0700

    Initial commit

---
+++ safeMint/safeMint/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\linaniva\documents\visual studio 2017\Projects\safeMint\safeMint\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
