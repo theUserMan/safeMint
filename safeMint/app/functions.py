55ac406e3580175e1ab16c026f288c5f4f0c2a8d
GENHEN <genhen3@gmail.com>
2018-04-04 00:36:59 +0000

    changing test cases in main

--- safeMint/app/functions.py
+++ safeMint/app/functions.py
@@ -1,26 +1,25 @@
 # command to run in bash/cmd:
 # python3.exe functions.py
 
-
-# from coinbase.wallet.client import Client
-import coinbase
+from coinbase.wallet.client import Client
 import sys
 
 # this function will use our API that we have for coinbase.com, ricky created
 # an account and got an API key. It queries coinbase's server for the price of
-# Currency1/Currency2 divisor.
+# currency_from/currency_to divisor.
 # It uses the API from:
 # developers.coinbase.com/docs/wallet/guides/price-data
 # to get data on cryptocurrencies
 def get_price_from_coinbase(date, currency_from, currency_to):
-	coinbase = coinbase.Coinbase.with_api_key("CqUy59ynpeYzRwXN", "ECGuB2sH7xYcuq1mDXt2IvANWxXECH8U")
-	# client = Client("CqUy59ynpeYzRwXN", "ECGuB2sH7xYcuq1mDXt2IvANWxXECH8U", api_version='2018-04-02')
+	client = Client("CqUy59ynpeYzRwXN", "ECGuB2sH7xYcuq1mDXt2IvANWxXECH8U", api_version='2018-04-02')
 
-	# the main function is coinbase.get_exchange_rate(c1, c2). Will return c1/c2
-	# in precision Documentation can be read at:
-	# https://github.com/resy/coinbase_python3
-	print('Current bitcoin price in {}/{} = {}'.format(currency_from, currency_to, coinbase.get_exchange_rate(currency_from, currency_to)))
-
+	# the main function is coinbase.get_spot_price(c1, c2). Will return c1/c2
+	# in double precision. Documentation can be read at:
+	# https://github.com/coinbase/coinbase-python
+	# Also, you need to feed 'c1-c2' into get_spot_price(), but you shouldn't
+	# convert from smaller value to bigger value. No 'USD-BTC'
+	# print('Current bitcoin price in {}/{} = {}'.format(currency_from, currency_to, client.get_spot_price(currency_pair = (currency_from + '-' + currency_to), date='2017-12-20').amount))
+	return client.get_spot_price(currency_pair = (currency_from + '-' + currency_to), date='2017-12-20').amount
 
 # Reading the file given from the arg
 def readFile(f):
@@ -31,8 +30,9 @@ def readFile(f):
 
 # print command line arguments
 def main():
-	get_price_from_coinbase("2018", "USD", "USD")
-	get_price_from_coinbase(2018, "USD", "USD")
+	# print('hello')
+	print('price at \'{}\''.format(get_price_from_coinbase("2017-12-20", "BTC", "USD")))
+	# get_price_from_coinbase(2018, "USD", "USD")
 
 	# print command line arguments
 	# for arg in sys.argv[1:]:
